be67b58fc6ddf9c90a286fa6563cb64b04243cb5
Vodkamishka <44522605+Vodkamishka@users.noreply.github.com>
2018-11-18 13:20:28 +0700

    nemo
    nemo

---
+++ nemo/Nemo.js
@@ -0,0 +1,44 @@
+﻿
+/*Загружем морских обитателей в массив*/
+
+var fish =[document.getElementById('character1'),
+           document.getElementById('character2'),
+           document.getElementById('character3'),
+           document.getElementById('character4'),
+           document.getElementById('character5'),
+           document.getElementById('character6'),
+           document.getElementById('character7'),
+           document.getElementById('character8'),
+           document.getElementById('character9'),
+           document.getElementById('character10'),
+           document.getElementById('character11'),
+           document.getElementById('character12'),
+           document.getElementById('character13')
+]
+
+/*Функция перезагрузки страницы*/
+
+function reload() {
+    document.location.reload(true);
+}
+
+/*Функция появления морского обитателя*/
+
+function appearanceFish() {
+    
+     var randomNumber = Math.floor(Math.random()*fish.length);  // В переменную записывается случайное число от 1 до 13 по количеству персонажей;
+      
+     fish[randomNumber].style.zIndex = "3"; // Случайному элементу из массива с морскими обитателями присваивается свойство видимости 3 (изначально у всех присвоено -1,чтобы не отображаться в браузере);
+    
+     var randomLeft = Math.floor(Math.random()*1000); // В переменную записывается случайное положение на экране по ширине 1000px (не смог найти функцию для определения ширины устройства, поэтому взял усредненное значение 1000)ж
+    
+    fish[randomNumber].style.left = randomLeft + "px"; // Морской обитатель появляется в случаной месте на экране;
+
+}
+
+/*Запуск функции появления морского обитателя*/
+
+appearanceFish();
+
+
+
