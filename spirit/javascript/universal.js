3dc142e98d84142a74408e0529f8f9ea0bbb4c09
Vodkamishka <44522605+Vodkamishka@users.noreply.github.com>
2018-11-30 17:39:46 +0700

    spirit
    spirit

---
+++ spirit/javascript/universal.js
@@ -0,0 +1,21 @@
+﻿   
+// Меню
+
+function changeMenu(event){
+    event.classList.toggle('change');
+}
+
+// Меню li
+
+$("li:nth-child(1)").on("click",function(){window.location = "../index.html#header"});
+
+$("li:nth-child(2)").on("click",function(){window.location = "../index.html#main"});
+
+$("li:nth-child(3)").on("click",function(){window.location = "../index.html#next"});
+
+$("li:nth-child(4)").on("click",function(){window.location = "../index.html#next2"});
+
+$("li:nth-child(5)").on("click",function(){window.location = "../index.html#next3"});
+
+$("li:nth-child(6)").on("click",function(){window.location = "../index.html#next4"});
+
