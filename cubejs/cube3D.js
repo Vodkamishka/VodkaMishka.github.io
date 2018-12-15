3a63e281f65e3adfb9c1bd9c4e57a3c8f8b942dc
Vodkamishka <44522605+Vodkamishka@users.noreply.github.com>
2018-12-15 16:06:34 +0700

    for detscop

--- cubejs/cube3D.js
+++ cubejs/cube3D.js
@@ -21,20 +21,3 @@ document.onmousemove = function(event) {
     
 }
 
-document.touchmove = function(event) {
-    
-    console.log(event);
-    
-    rotateX = event.pageX;
-     rotateY = event.pageY;
-    
-    block.style.transform = "rotateY(" + rotateX + "deg)" + "rotateX(" + -rotateY + "deg)";
-    
-    
-    
-    
-    console.log(rotateX );
-    
-    
-    
-}
