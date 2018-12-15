eab313220968e50cae9e9429fb014ba039bcc3a9
Vodkamishka <44522605+Vodkamishka@users.noreply.github.com>
2018-12-15 16:01:24 +0700

    touchmove
    touchmove

--- cubejs/cube3D.js
+++ cubejs/cube3D.js
@@ -1,9 +1,27 @@
 ﻿
 let block = document.querySelector(".block");
 
-let side = document.querySelectorAll("side");
+
 
 document.onmousemove = function(event) {
+    
+    console.log(event);
+    
+    rotateX = event.pageX;
+     rotateY = event.pageY;
+    
+    block.style.transform = "rotateY(" + rotateX + "deg)" + "rotateX(" + -rotateY + "deg)";
+    
+    
+    
+    
+    console.log(rotateX );
+    
+    
+    
+}
+
+document.touchmove = function(event) {
     
     console.log(event);
     
